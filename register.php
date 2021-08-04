efebb4c7a0ab1f3ef7559631a7334f11c63f182b
14Martin14 <88460793+14Martin14@users.noreply.github.com>
2021-08-05 00:06:45 +0200

    added file

---
+++ register.php
@@ -0,0 +1,26 @@
+<?php
+echo "My first PHP script!";
+?>
+
+<meta charset="Utf-8">
+
+<body>
+    <form action="regisztralas.php" method="POSt">
+        E-mail : <input type="email" name="gmail" /> <br />
+        Felhasználónév: <input type="text" name="username" /><br />
+        Jelszó: <input type="password" name="jelszo" /><br />
+        <input type="submit" value="Regisztráció" name="submit" /> <br /><br />
+
+
+    </form>
+</body>
+<style>
+    body {
+      background-image: url('https://th.bing.com/th/id/R.b054350d36ea079c37b351b25561b72e?rik=rkRwO0UQRYTkgQ&pid=ImgRaw');
+      background-repeat: fixed;
+      background-attachment:fixed;
+      background-size: cover;
+    }
+    </style>
+
+</html>
